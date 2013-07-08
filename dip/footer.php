b3b5f580deb45734fe7f6a4179665586f181b2d4
Diego Paraizo <diego@paraizo.org>
2013-07-08 17:53:27 -0300

    format and add sidebar to footer

--- dip/footer.php
+++ dip/footer.php
@@ -6,19 +6,28 @@
  *
  * @package dip
  */
- 
- /** Get theme info */
- global $dip; ?>
-  <footer class="row" role="contentinfo">
-    <div class="large-12 columns">
-      <hr />
-      <div class="row">
-        <div class="large-6 small 12 columns columns">
-          <p>&copy; This is version <strong><?php echo $dip->theme->version; ?></strong> from the <?php echo $dip->theme->name; ?></p>
-        </div>
-        <div class="large-6 small-12 columns">
-          <?php dp_menu('footer', array('attr' => array('class' => 'inline-list right')) ); ?>
-        </div>
+
+global $dip; ?>
+
+  <aside id="footer-widgets" class="widgets hide-for-small">
+    <div class="row">
+      <div class="large-12 columns">
+      <?php if ( is_active_sidebar('footer') ) {
+        echo '<ul class="large-block-grid-5">';
+        dynamic_sidebar('footer');
+        echo '</ul>';
+      } ?>
+      </div>
+    </div>
+  </aside>
+
+  <footer id="footer">
+    <div class="row">
+      <div class="large-8 small-12 push-4 columns">
+       <?php dp_menu('footer', array('attr' => array('class' => 'inline-list right')) ); ?>
+      </div>
+      <div class="large-4 small-12 pull-8 columns">
+        <p><?php echo $dip->theme->parent()->name; ?> &copy; 2013 <?php echo $dip->theme->parent()->author; ?></p>
       </div>
     </div>
   </footer>
