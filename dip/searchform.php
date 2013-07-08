d75753919c599b175bef7df389d2412b1611231b
Diego Paraizo <diego@paraizo.org>
2013-07-08 10:52:25 -0300

    adjust search form size

--- dip/searchform.php
+++ dip/searchform.php
@@ -6,10 +6,10 @@
  */
 ?>
 <form method="get" id="searchform" class="searchform row collapse" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-  <div class="small-10 columns">
+  <div class="small-9 columns">
     <input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'dip' ); ?>" />
   </div>
-  <div class="small-2 columns">
+  <div class="small-3 columns">
 		<input type="submit" class="button prefix" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'dip' ); ?>" />
   </div>
 </form>
