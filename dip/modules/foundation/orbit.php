479cf129b00508d95b5ee8da748b8d6372a09005
Diego Paraizo <diego@paraizo.org>
2013-07-09 16:00:34 -0300

    update orbit function to foundation 4.0

--- dip/modules/foundation/orbit.php
+++ dip/modules/foundation/orbit.php
@@ -47,74 +47,55 @@ class DP_Foundation_Orbit
     }
     else
     {
+      // add orbit preloader
+      $this->html->find('div', 0)->innertext .= '<div class="preloader"></div>';
+      
+      // add slides wrapper and reload the parser
+      $this->html->find('div', 0)->innertext .= '<ul data-orbit></ul>';
+      $this->html = str_get_html($this->html);
+      
       foreach ($this->nodes as $slide)
-        $this->html->find('div', 0)->innertext .= "<img src=\"{$slide['image']}\" />";
+        $this->html->find('ul', 0)->innertext .= "<li><img src=\"{$slide['image']}\" /></li>";
     }
 
     // print
     echo $this->html;
   }
 }
+
 /** load panel only in wp-admin */
 if(!is_admin()) return;
-
 new DP_Panel_Orbit;
+
 class DP_Panel_Orbit extends DP_Panel
 {
   public function init() {
-        $this->name      = __('Orbit Slider');
-        $this->namespace = 'orbit';
-        $this->module    = 'foundation';
+    $this->name      = __('Orbit Slider');
+    $this->namespace = 'orbit';
+    $this->module    = 'foundation';
 
-        $this->tabs      = array( 'slides' => 'Slides',
-                                  'settings' => 'Settings' );
-    }
+    $this->parent    = false;
+    $this->position  = 23.05;
+    $this->icon      = 'rectangles';
+  }
     
-    public function save_settings()
-    {
-        $this->settings = get_option( $this->namespace );
-        
-        switch ( $_GET['tab'] ) {
-            case 'slides':
-                
-                function _filter($var) { return ( !empty($var['image']) || !empty($var['desc']) ); }
-                $slides = array_filter($_POST['slide'], '_filter') ;
+  public function save_settings()
+  {
+    $this->settings = get_option( $this->namespace );
 
-                $this->settings['slides']      = $slides; //array_merge( $slides );
-                
-            /*    
-                $logo = $_POST['one-logo'];
-                $imgsize = getimagesize($_POST['one-logo']['image']);
-                
-                $logo['image-w'] = $imgsize[0];
-                $logo['image-h'] = $imgsize[1];
-                
-                $this->settings['one-logo'] = $logo;
-                $this->settings['one-colors'] = $_POST['one-colors'];
-                $this->settings['one-header'] = $_POST['one-header']; */
-                break;
-            
-            default:
-                // filter social links
-            /*    function _filter_sl($var) { return ( !empty($var['title']) && !empty($var['url']) && !empty($var['network']) ); }
-                $sl = array_filter($_POST['one-sl'], '_filter_sl') ;
-                
-                $this->settings['one-profile'] = $_POST['one-profile'];
-                $this->settings['one-sl']      = array_merge( $sl );
-                $this->settings['one-footer']  = $this->_filter( $_POST['one-footer'] );
-                $this->settings['one-ga']      = $this->_filter( $_POST['one-ga'] ); */
-                break;
-        }
-        
-        update_option( $this->namespace, $this->settings );
-    }
-    
-    public function enqueue_scripts()
-    {
-      global $dip;
-        wp_enqueue_style('thickbox');
-        wp_enqueue_script('media-upload');
-        
-        wp_enqueue_script( 'dip', $dip->theme->template_directory_uri . '/assets/wp-admin.js', array( 'wp-color-picker' ), false, true );
-    }
+    function _filter($var) { return ( !empty($var['image']) || !empty($var['desc']) ); }
+    $slides = array_filter($_POST['slide'], '_filter') ;
+
+    $this->settings['slides']      = $slides; //array_merge( $slides );
+    update_option( $this->namespace, $this->settings );
+  }
+
+  public function enqueue_scripts()
+  {
+    global $dip;
+    wp_enqueue_style('thickbox');
+    wp_enqueue_script('media-upload');
+
+    wp_enqueue_script( 'dip', $dip->theme->template_directory_uri . '/assets/wp-admin.js', array( 'wp-color-picker' ), false, true );
+  }
 }
