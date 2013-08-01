cd87327339ccef7a6f83ffd6b18ad525f904b0ae
Diego Paraizo <diego@paraizo.org>
2013-08-01 18:16:26 -0300

    finalize sendmail module

--- dip/modules/sendmail.php
+++ dip/modules/sendmail.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * The helper for displaying Google Maps
+ * Module to help send email forms
  *
  * @package Dip Framework
- * @subpackage Gmaps Module
+ * @subpackage Sendmail Module
  * @version 1.0.0
  * @since Dip Framework 1.0
 */
@@ -13,9 +13,22 @@ if(is_admin() || $_SERVER['REQUEST_METHOD'] != "POST" || !wp_verify_nonce($_POST
 global $dip;
 $config = $dip->modules['sendmail'];
 
-$headers[] = 'From: Me Myself <me@example.net>';
+$to = $config['to'];
+$headers[] = "From: {$_POST['name']} <{$_POST['email']}>";
+$subject = $_POST['subject'];
+$message = '';
 
-wp_mail($config['to'], '$subject', $_POST['name'], $headers);
+$fields = $_POST;
+unset($fields['_wpnonce']);
+unset($fields['_wp_http_referer']);
+unset($fields['subject']);
 
-wp_redirect($_SERVER["HTTP_REFERER"].'?messagesend');
+foreach($fields as $field => $value)
+{
+  $message .= ucfirst($field) . ': ' . $value . "<br />"; 
+}
+
+wp_mail($to, $subject, $message, $headers);
+
+wp_redirect($_SERVER["HTTP_REFERER"].'?success');
 exit;
